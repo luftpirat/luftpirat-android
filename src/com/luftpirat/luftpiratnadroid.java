0c6b50710266bf77b8b4c17026467f8e0ee63af0
luftpirat <luftpirat512-git@yahoo.de>
2011-07-10 11:05:28 +0200

    commit test

--- src/com/luftpirat/luftpiratnadroid.java
+++ src/com/luftpirat/luftpiratnadroid.java
@@ -9,5 +9,6 @@ public class luftpiratnadroid extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // Hallo Welt!
     }
 }
